a9342c20357ca0b9357cadbc895ce98eb87729d9
John Clifford <johnclifford00@gmail.com>
2020-05-26 17:36:34 -0400

    add rules for scoring full house and small straight. fix rolling more than 3 times per turn

--- src/Die.js
+++ src/Die.js
@@ -1,18 +1,27 @@
-import React, { Component } from "react";
-import "./Die.css";
+import React, { Component } from 'react';
+import './Die.css';
 
 class Die extends Component {
-  render() {
-    return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
-      >
-        {this.props.val}
-      </button>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.handleDieClick = this.handleDieClick.bind(this);
+	}
+
+	handleDieClick() {
+		this.props.handleClick(this.props.idx);
+	}
+
+	render() {
+		return (
+			<button
+				className={'Die'}
+				style={{ backgroundColor: this.props.locked ? 'grey' : 'black' }}
+				onClick={this.handleDieClick}
+			>
+				{this.props.val}
+			</button>
+		);
+	}
 }
 
 export default Die;
